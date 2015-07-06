ef8f51235c0166be66b5ee7a3621bc763a11ac67
haya <hayasoft@hotmail.com>
2015-07-06 21:08:03 +0900

    新しいモジュールAPI(V2)に対応

--- mod_pginfo_sqlite.c
+++ mod_pginfo_sqlite.c
@@ -7,8 +7,11 @@
 #include <windows.h>
 #include <stdio.h>
 #include <shlwapi.h>
+#include <inttypes.h>
 
 #include "sqlite3.h"
+
+#define IN_SHARED_MODULE
 #include "modules_def.h"
 
 static WCHAR *db_fname = NULL;
@@ -70,24 +73,28 @@ static int db_search(sqlite3 *dbh, const ProgInfo *pi)
 	const char str[] = "SELECT * FROM programs WHERE chname = ? AND pgname = ? AND start = ?";
 	ret = sqlite3_prepare_v2(dbh, str, sizeof(str)+1, &stmt, NULL);
 	if (ret != SQLITE_OK) {
-		fwprintf(stderr, L"[ERROR] sqlite3_prepare_v2(): %s\n", sqlite3_errmsg16(dbh));
+		//fwprintf(stderr, L"[ERROR] sqlite3_prepare_v2(): %s\n", sqlite3_errmsg16(dbh));
+		output_message(MSG_ERROR, L"sqlite3_prepare_v2(): %s\n", sqlite3_errmsg16(dbh));
 		return 0;
 	}
 	ret = sqlite3_bind_text16(stmt, 1, pi->chname, pi->chnamelen*sizeof(WCHAR), SQLITE_STATIC);
 	if (ret != SQLITE_OK) {
-		fwprintf(stderr, L"[ERROR] sqlite3_bind_text16(): %s\n", sqlite3_errmsg16(dbh));
+		//fwprintf(stderr, L"[ERROR] sqlite3_bind_text16(): %s\n", sqlite3_errmsg16(dbh));
+		output_message(MSG_ERROR, L"sqlite3_bind_text16(): %s\n", sqlite3_errmsg16(dbh));
 		sqlite3_finalize(stmt);
 		return 0;
 	}
 	ret = sqlite3_bind_text16(stmt, 2, pi->pname, pi->pnamelen*sizeof(WCHAR), SQLITE_STATIC);
 	if (ret != SQLITE_OK) {
-		fwprintf(stderr, L"[ERROR] sqlite3_bind_text16(): %s\n", sqlite3_errmsg16(dbh));
+		//fwprintf(stderr, L"[ERROR] sqlite3_bind_text16(): %s\n", sqlite3_errmsg16(dbh));
+		output_message(MSG_ERROR, L"sqlite3_bind_text16(): %s\n", sqlite3_errmsg16(dbh));
 		sqlite3_finalize(stmt);
 		return 0;
 	}
 	ret = sqlite3_bind_int64(stmt, 3, timenum14_start(pi));
 	if (ret != SQLITE_OK) {
-		fwprintf(stderr, L"[ERROR] sqlite3_bind_int64(): %s\n", sqlite3_errmsg16(dbh));
+		//fwprintf(stderr, L"[ERROR] sqlite3_bind_int64(): %s\n", sqlite3_errmsg16(dbh));
+		output_message(MSG_ERROR, L"sqlite3_bind_int64(): %s\n", sqlite3_errmsg16(dbh));
 		sqlite3_finalize(stmt);
 		return 0;
 	}
@@ -132,108 +139,132 @@ static int db_insert(sqlite3 *dbh, const WCHAR *ts_path, const ProgInfo *pi, con
 	PathStripPath(ts_fname);
 
 	if( sqlite3_prepare_v2(dbh, str1, sizeof(str1) + 1, &stmt, NULL) != SQLITE_OK ) {
-		fwprintf(stderr, L"[ERROR] sqlite3_prepare_v2(): %s\n", sqlite3_errmsg16(dbh));
+		//fwprintf(stderr, L"[ERROR] sqlite3_prepare_v2(): %s\n", sqlite3_errmsg16(dbh));
+		output_message(MSG_ERROR, L"sqlite3_prepare_v2(): %s\n", sqlite3_errmsg16(dbh));
 		return 0;
 	}
 	if( sqlite3_bind_text16(stmt, 1, pi->chname, pi->chnamelen*sizeof(WCHAR), SQLITE_STATIC) != SQLITE_OK ) {
-		fwprintf(stderr, L"[ERROR] sqlite3_bind_text16(): %s\n", sqlite3_errmsg16(dbh));
+		//fwprintf(stderr, L"[ERROR] sqlite3_bind_text16(): %s\n", sqlite3_errmsg16(dbh));
+		output_message(MSG_ERROR, L"sqlite3_bind_text16(): %s\n", sqlite3_errmsg16(dbh));
 		goto END;
 	}
 	if( sqlite3_bind_int(stmt, 2, pi->chnum) != SQLITE_OK ) {
-		fwprintf(stderr, L"[ERROR] sqlite3_bind_int(): %s\n", sqlite3_errmsg16(dbh));
+		//fwprintf(stderr, L"[ERROR] sqlite3_bind_int(): %s\n", sqlite3_errmsg16(dbh));
+		output_message(MSG_ERROR, L"sqlite3_bind_int(): %s\n", sqlite3_errmsg16(dbh));
 		goto END;
 	}
 	if( sqlite3_bind_text16(stmt, 3, pi->pname, pi->pnamelen*sizeof(WCHAR), SQLITE_STATIC) != SQLITE_OK ) {
-		fwprintf(stderr, L"[ERROR] sqlite3_bind_text16(): %s\n", sqlite3_errmsg16(dbh));
+		//fwprintf(stderr, L"[ERROR] sqlite3_bind_text16(): %s\n", sqlite3_errmsg16(dbh));
+		output_message(MSG_ERROR, L"sqlite3_bind_text16(): %s\n", sqlite3_errmsg16(dbh));
 		goto END;
 	}
 	if( sqlite3_bind_int64(stmt, 4, timenum14_start(pi)) != SQLITE_OK ) {
-		fwprintf(stderr, L"[ERROR] sqlite3_bind_int64(): %s\n", sqlite3_errmsg16(dbh));
+		//fwprintf(stderr, L"[ERROR] sqlite3_bind_int64(): %s\n", sqlite3_errmsg16(dbh));
+		output_message(MSG_ERROR, L"sqlite3_bind_int64(): %s\n", sqlite3_errmsg16(dbh));
 		goto END;
 	}
 	if( sqlite3_bind_int64(stmt, 5, timenum14_dur(pi)) != SQLITE_OK ) {
-		fwprintf(stderr, L"[ERROR] sqlite3_bind_int64(): %s\n", sqlite3_errmsg16(dbh));
+		//fwprintf(stderr, L"[ERROR] sqlite3_bind_int64(): %s\n", sqlite3_errmsg16(dbh));
+		output_message(MSG_ERROR, L"sqlite3_bind_int64(): %s\n", sqlite3_errmsg16(dbh));
 		goto END;
 	}
 	if( sqlite3_bind_text16(stmt, 6, genre_str, 1024*sizeof(WCHAR), SQLITE_STATIC) != SQLITE_OK ) {
-		fwprintf(stderr, L"[ERROR] sqlite3_bind_text16(): %s\n", sqlite3_errmsg16(dbh));
+		//fwprintf(stderr, L"[ERROR] sqlite3_bind_text16(): %s\n", sqlite3_errmsg16(dbh));
+		output_message(MSG_ERROR, L"sqlite3_bind_text16(): %s\n", sqlite3_errmsg16(dbh));
 		goto END;
 	}
 	if( sqlite3_bind_blob(stmt, 7, genre, 4, SQLITE_STATIC) != SQLITE_OK ) {
-		fwprintf(stderr, L"[ERROR] sqlite3_bind_blob(): %s\n", sqlite3_errmsg16(dbh));
+		//fwprintf(stderr, L"[ERROR] sqlite3_bind_blob(): %s\n", sqlite3_errmsg16(dbh));
+		output_message(MSG_ERROR, L"sqlite3_bind_blob(): %s\n", sqlite3_errmsg16(dbh));
 		goto END;
 	}
 	if( sqlite3_bind_text16(stmt, 8, pi->pdetail, pi->pdetaillen*sizeof(WCHAR), SQLITE_STATIC) != SQLITE_OK ) {
-		fwprintf(stderr, L"[ERROR] sqlite3_bind_text16(): %s\n", sqlite3_errmsg16(dbh));
+		//fwprintf(stderr, L"[ERROR] sqlite3_bind_text16(): %s\n", sqlite3_errmsg16(dbh));
+		output_message(MSG_ERROR, L"sqlite3_bind_text16(): %s\n", sqlite3_errmsg16(dbh));
 		goto END;
 	}
 	if( sqlite3_bind_text16(stmt, 9, pi->pextend, pi->pextendlen*sizeof(WCHAR), SQLITE_STATIC) != SQLITE_OK ) {
-		fwprintf(stderr, L"[ERROR] sqlite3_bind_text16(): %s\n", sqlite3_errmsg16(dbh));
+		//fwprintf(stderr, L"[ERROR] sqlite3_bind_text16(): %s\n", sqlite3_errmsg16(dbh));
+		output_message(MSG_ERROR, L"sqlite3_bind_text16(): %s\n", sqlite3_errmsg16(dbh));
 		goto END;
 	}
 
 	if( sqlite3_step(stmt) != SQLITE_DONE ) {
-		fwprintf(stderr, L"[ERROR] sqlite3_step(): %s\n", sqlite3_errmsg16(dbh));
+		//fwprintf(stderr, L"[ERROR] sqlite3_step(): %s\n", sqlite3_errmsg16(dbh));
+		output_message(MSG_ERROR, L"sqlite3_step(): %s\n", sqlite3_errmsg16(dbh));
 		goto END;
 	}
 	if( sqlite3_reset(stmt) != SQLITE_OK ) {
-		fwprintf(stderr, L"[ERROR] sqlite3_reset(): %s\n", sqlite3_errmsg16(dbh));
+		//fwprintf(stderr, L"[ERROR] sqlite3_reset(): %s\n", sqlite3_errmsg16(dbh));
+		output_message(MSG_ERROR, L"sqlite3_reset(): %s\n", sqlite3_errmsg16(dbh));
 		goto END;
 	}
 	if( sqlite3_finalize( stmt ) != SQLITE_OK ) {
-		fwprintf(stderr, L"[ERROR] sqlite3_finalize(): %s\n", sqlite3_errmsg16(dbh));
+		//fwprintf(stderr, L"[ERROR] sqlite3_finalize(): %s\n", sqlite3_errmsg16(dbh));
+		output_message(MSG_ERROR, L"sqlite3_finalize(): %s\n", sqlite3_errmsg16(dbh));
 		return 0;
 	}
 	id = sqlite3_last_insert_rowid(dbh);
 
 	if( sqlite3_prepare_v2(dbh, str2, sizeof(str2) + 1, &stmt, NULL) != SQLITE_OK ) {
-		fwprintf(stderr, L"[ERROR] sqlite3_prepare_v2(): %s\n", sqlite3_errmsg16(dbh));
+		//fwprintf(stderr, L"[ERROR] sqlite3_prepare_v2(): %s\n", sqlite3_errmsg16(dbh));
+		output_message(MSG_ERROR, L"sqlite3_prepare_v2(): %s\n", sqlite3_errmsg16(dbh));
 		return 0;
 	}
 	if( sqlite3_bind_int64(stmt, 1, id) != SQLITE_OK ) {
-		fwprintf(stderr, L"[ERROR] sqlite3_bind_int64(): %s\n", sqlite3_errmsg16(dbh));
+		//fwprintf(stderr, L"[ERROR] sqlite3_bind_int64(): %s\n", sqlite3_errmsg16(dbh));
+		output_message(MSG_ERROR, L"sqlite3_bind_int64(): %s\n", sqlite3_errmsg16(dbh));
 		goto END;
 	}
 	if( sqlite3_bind_text16(stmt, 2, ts_fname, -1, SQLITE_STATIC) != SQLITE_OK ) {
-		fwprintf(stderr, L"[ERROR] sqlite3_bind_text16(): %s\n", sqlite3_errmsg16(dbh));
+		//fwprintf(stderr, L"[ERROR] sqlite3_bind_text16(): %s\n", sqlite3_errmsg16(dbh));
+		output_message(MSG_ERROR, L"sqlite3_bind_text16(): %s\n", sqlite3_errmsg16(dbh));
 		goto END;
 	}
 	if( sqlite3_bind_text16(stmt, 3, ci->tuner_name, -1, SQLITE_STATIC) != SQLITE_OK ) {
-		fwprintf(stderr, L"[ERROR] sqlite3_bind_text16(): %s\n", sqlite3_errmsg16(dbh));
+		//fwprintf(stderr, L"[ERROR] sqlite3_bind_text16(): %s\n", sqlite3_errmsg16(dbh));
+		output_message(MSG_ERROR, L"sqlite3_bind_text16(): %s\n", sqlite3_errmsg16(dbh));
 		goto END;
 	}
 	if( sqlite3_bind_text16(stmt, 4, ci->sp_str, -1, SQLITE_STATIC) != SQLITE_OK ) {
-		fwprintf(stderr, L"[ERROR] sqlite3_bind_text16(): %s\n", sqlite3_errmsg16(dbh));
+		//fwprintf(stderr, L"[ERROR] sqlite3_bind_text16(): %s\n", sqlite3_errmsg16(dbh));
+		output_message(MSG_ERROR, L"sqlite3_bind_text16(): %s\n", sqlite3_errmsg16(dbh));
 		goto END;
 	}
 	if( sqlite3_bind_int(stmt, 5, ci->sp_num) != SQLITE_OK ) {
-		fwprintf(stderr, L"[ERROR] sqlite3_bind_int(): %s\n", sqlite3_errmsg16(dbh));
+		//fwprintf(stderr, L"[ERROR] sqlite3_bind_int(): %s\n", sqlite3_errmsg16(dbh));
+		output_message(MSG_ERROR, L"sqlite3_bind_int(): %s\n", sqlite3_errmsg16(dbh));
 		goto END;
 	}
 	if( sqlite3_bind_text16(stmt, 6, ci->ch_str, -1, SQLITE_STATIC) != SQLITE_OK ) {
-		fwprintf(stderr, L"[ERROR] sqlite3_bind_text16(): %s\n", sqlite3_errmsg16(dbh));
+		//fwprintf(stderr, L"[ERROR] sqlite3_bind_text16(): %s\n", sqlite3_errmsg16(dbh));
+		output_message(MSG_ERROR, L"sqlite3_bind_text16(): %s\n", sqlite3_errmsg16(dbh));
 		goto END;
 	}
 	if( sqlite3_bind_int(stmt, 7, ci->ch_num) != SQLITE_OK ) {
-		fwprintf(stderr, L"[ERROR] sqlite3_bind_int(): %s\n", sqlite3_errmsg16(dbh));
+		//fwprintf(stderr, L"[ERROR] sqlite3_bind_int(): %s\n", sqlite3_errmsg16(dbh));
+		output_message(MSG_ERROR, L"sqlite3_bind_int(): %s\n", sqlite3_errmsg16(dbh));
 		goto END;
 	}
 	if (ci->service_id >= 0) {
 		if (sqlite3_bind_int(stmt, 8, ci->service_id) != SQLITE_OK) {
-			fwprintf(stderr, L"[ERROR] sqlite3_bind_int(): %s\n", sqlite3_errmsg16(dbh));
+			//fwprintf(stderr, L"[ERROR] sqlite3_bind_int(): %s\n", sqlite3_errmsg16(dbh));
+			output_message(MSG_ERROR, L"sqlite3_bind_int(): %s\n", sqlite3_errmsg16(dbh));
 			goto END;
 		}
 	}
 
 	if( sqlite3_step(stmt) != SQLITE_DONE ) {
-		fwprintf(stderr, L"[ERROR] sqlite3_step(): %s\n", sqlite3_errmsg16(dbh));
+		//fwprintf(stderr, L"[ERROR] sqlite3_step(): %s\n", sqlite3_errmsg16(dbh));
+		output_message(MSG_ERROR, L"sqlite3_step(): %s\n", sqlite3_errmsg16(dbh));
 		goto END;
 	}
 
 	retval = 1;
 END:
 	if( sqlite3_finalize( stmt ) != SQLITE_OK ) {
-		fwprintf(stderr, L"[ERROR] sqlite3_finalize(): %s\n", sqlite3_errmsg16(dbh));
+		//fwprintf(stderr, L"[ERROR] sqlite3_finalize(): %s\n", sqlite3_errmsg16(dbh));
+		output_message(MSG_ERROR, L"sqlite3_finalize(): %s\n", sqlite3_errmsg16(dbh));
 		retval = 0;
 	}
 	return retval;
@@ -255,41 +286,52 @@ static void hook_pgoutput_close(void *ps, const ProgInfo* pi)
 
 	ret = sqlite3_open16(db_fname, &dbh);
 	if (ret != SQLITE_OK) {
-		fwprintf(stderr, L"[ERROR] sqlite3_open16(): %s\n", sqlite3_errmsg16(dbh));
-		fprintf(stderr, "[ERROR] sqliteデータベースのオープンに失敗しました\n");
+		//fwprintf(stderr, L"[ERROR] sqlite3_open16(): %s\n", sqlite3_errmsg16(dbh));
+		//fprintf(stderr, "[ERROR] sqliteデータベースのオープンに失敗しました\n");
+		output_message(MSG_ERROR, L"sqlite3_open16(): %s\n", sqlite3_errmsg16(dbh));
+		output_message(MSG_ERROR, L"sqliteデータベースのオープンに失敗しました\n");
 		goto END2;
 	}
 	sqlite3_busy_timeout(dbh, 10 * 1000); /* 10s */
 	ret = sqlite3_exec(dbh, "PRAGMA synchronous=1;", NULL, NULL, NULL);
 	if (ret != SQLITE_OK) {
-		fwprintf(stderr, L"[ERROR] sqlite3_exec(): %s\n", sqlite3_errmsg16(dbh));
-		fprintf(stderr, "[ERROR] データベースを synchronous=1 に設定できませんでした\n");
+		//fwprintf(stderr, L"[ERROR] sqlite3_exec(): %s\n", sqlite3_errmsg16(dbh));
+		//fprintf(stderr, "[ERROR] データベースを synchronous=1 に設定できませんでした\n");
+		output_message(MSG_ERROR, L"sqlite3_exec(): %s\n", sqlite3_errmsg16(dbh));
+		output_message(MSG_ERROR, L"データベースを synchronous=1 に設定できませんでした\n");
 		goto END2;
 	}
 	ret = sqlite3_exec(dbh, "BEGIN IMMEDIATE;", NULL, NULL, NULL); /* BEGIN DEFFERED; (デフォルト) ではだめ */
 	if (ret != SQLITE_OK) {
-		fwprintf(stderr, L"[ERROR] sqlite3_exec(): %s\n", sqlite3_errmsg16(dbh));
-		fprintf(stderr, "[ERROR] sqliteのトランザクション開始に失敗しました\n");
+		//fwprintf(stderr, L"[ERROR] sqlite3_exec(): %s\n", sqlite3_errmsg16(dbh));
+		//fprintf(stderr, "[ERROR] sqliteのトランザクション開始に失敗しました\n");
+		output_message(MSG_ERROR, L"sqlite3_exec(): %s\n", sqlite3_errmsg16(dbh));
+		output_message(MSG_ERROR, L"sqliteのトランザクション開始に失敗しました\n");
 		goto END2;
 	}
 	ret = db_search(dbh, pi);
 	if (ret == 0) {
-		fprintf(stderr, "[ERROR] sqliteデータベースの検索に失敗しました\n");
+		//fprintf(stderr, "[ERROR] sqliteデータベースの検索に失敗しました\n");
+		output_message(MSG_ERROR, L"sqliteデータベースの検索に失敗しました\n");
 		goto END1;
 	} else if (ret == 1) {
 		ret = db_insert(dbh, pstat->fn_ts, pi, &pstat->ch_info);
 		if (!ret) {
-			fprintf(stderr, "[ERROR] sqliteデータベースの追加に失敗しました\n");
+			//fprintf(stderr, "[ERROR] sqliteデータベースの追加に失敗しました\n");
+			output_message(MSG_ERROR, L"sqliteデータベースの追加に失敗しました\n");
 			goto END1;
 		}
 	} else {
-		fprintf(stderr, "[WARN] sqliteデータベースにすでに番組が登録済みです\n");
+		//fprintf(stderr, "[WARN] sqliteデータベースにすでに番組が登録済みです\n");
+		output_message(MSG_WARNING, L"sqliteデータベースにすでに番組が登録済みです\n");
 		goto END1;
 	}
 	ret = sqlite3_exec(dbh, "COMMIT;", NULL, NULL, NULL);
 	if (ret != SQLITE_OK) {
-		fwprintf(stderr, L"[ERROR] sqlite3_exec(): %s\n", sqlite3_errmsg16(dbh));
-		fprintf(stderr, "[ERROR] sqliteのコミットに失敗しました\n");
+		//fwprintf(stderr, L"[ERROR] sqlite3_exec(): %s\n", sqlite3_errmsg16(dbh));
+		//fprintf(stderr, "[ERROR] sqliteのコミットに失敗しました\n");
+		output_message(MSG_ERROR, L"sqlite3_exec(): %s\n", sqlite3_errmsg16(dbh));
+		output_message(MSG_ERROR, L"sqliteのコミットに失敗しました\n");
 	} else {
 		commit_ok = 1;
 	}
@@ -297,18 +339,23 @@ END1:
 	if (!commit_ok) {
 		ret = sqlite3_exec(dbh, "ROLLBACK;", NULL, NULL, NULL);
 		if (ret != SQLITE_OK) {
-			fwprintf(stderr, L"[ERROR] sqlite3_exec(): %s\n", sqlite3_errmsg16(dbh));
-			fprintf(stderr, "[ERROR] sqliteのロールバックに失敗しました\n");
+			//fwprintf(stderr, L"[ERROR] sqlite3_exec(): %s\n", sqlite3_errmsg16(dbh));
+			//fprintf(stderr, "[ERROR] sqliteのロールバックに失敗しました\n");
+			output_message(MSG_ERROR, L"sqlite3_exec(): %s\n", sqlite3_errmsg16(dbh));
+			output_message(MSG_ERROR, L"sqliteのロールバックに失敗しました\n");
 		} else {
-			printf("[WARN] データベースをロールバックしました\n");
+			//printf("[WARN] データベースをロールバックしました\n");
+			output_message(MSG_WARNING, L"データベースをロールバックしました\n");
 		}
 	}
 END2:
 	/* openに失敗してもcloseする */
 	ret = sqlite3_close(dbh);
 	if (ret != SQLITE_OK) {
-		fwprintf(stderr, L"[ERROR] sqlite3_close(): %s\n", sqlite3_errmsg16(dbh));
-		fprintf(stderr, "[ERROR] sqliteデータベースのクローズに失敗しました\n");
+		//fwprintf(stderr, L"[ERROR] sqlite3_close(): %s\n", sqlite3_errmsg16(dbh));
+		//fprintf(stderr, "[ERROR] sqliteデータベースのクローズに失敗しました\n");
+		output_message(MSG_ERROR, L"sqlite3_close(): %s\n", sqlite3_errmsg16(dbh));
+		output_message(MSG_ERROR, L"sqliteデータベースのクローズに失敗しました\n");
 	}
 END3:
 	free(pstat->fn_ts);
@@ -352,12 +399,12 @@ static const WCHAR *set_db(const WCHAR *param)
 }
 
 static cmd_def_t cmds[] = {
-	{ L"-pgdb", L"sqlite DBファイル", 1, set_db },
+	{ L"--pgdb", L"sqlite DBファイル", 1, set_db },
 	NULL,
 };
 
-MODULE_EXPORT module_def_t mod_pginfo_sqlite = {
-	TSDUMP_MODULE_V1,
+MODULE_DEF module_def_t mod_pginfo_sqlite = {
+	TSDUMP_MODULE_V2,
 	L"mod_pginfo_sqlite",
 	register_hooks,
 	cmds
