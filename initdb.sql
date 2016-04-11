5685179f7a814fc2a05e09c4ee0420e4d829195d
haya <hayasoft@hotmail.com>
2016-04-11 09:10:44 +0900

    データベースの構成を変更

--- initdb.sql
+++ initdb.sql
@@ -1,5 +1,3 @@
 PRAGMA journal_mode=WAL;
-create table programs( id integer primary key, chname text, chnum integer, pgname text, start integer, duration integer, genre text, genre_raw blob, detail text, extended text );
-create table bonparams( id integer, tsname text, bontuname text, bonspname text, bonspnum integer, bonchname text, bonchnum integer, serviceid integer );
-create index programs_index on programs(chname, pgname, start);
-create index bonparams_index on bonparams(tsname);
+create table programs( id integer primary key, nid integer, tsid integer, service_id integer, service_name integer, service_name_raw blob, event_id integer, program_name text, program_name_raw blob, start integer, duration integer, genre text, genre_raw blob, program_text text, program_text_raw blob, program_detail text, program_detail_raw blob, record_filename text );
+create index programs_index on programs( nid, tsid, service_id, event_id, service_name, program_name, start, duration, record_filename );
